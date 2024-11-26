1e1bc3f554a8a1faa614cc79ef4f7a958e5e9cde
duw192 <dinhngocdu2005.mwg@gmail.com>
2024-11-27 00:58:15 +0700

    Update plot_data.py

--- src/visualization/plot_data.py
+++ src/visualization/plot_data.py
@@ -66,6 +66,33 @@ def show_chart3(data):
     plt.figure(figsize=(8, 8))
     plt.pie(students_above_80, labels=labels_vietnamese, autopct='%1.1f%%', startangle=140)
     plt.title('Số lượng học sinh có điểm trên 80 của từng môn học')
+    plt.show()1
+
+def show_chart4(data):
+    # Phân loại sinh viên có điểm số trên 80 (cho các môn học)
+    all_subjects = ['math_score', 'history_score', 'physics_score', 
+                    'chemistry_score', 'biology_score', 'english_score', 'geography_score']
+    # Lọc ra những sinh viên có điểm số trên 80 ở ít nhất một môn
+    df_above_80 = data[data[all_subjects].gt(80).any(axis=1)]
+    
+    # Tính số sinh viên có thời gian tự học > 15 giờ và <= 15 giờ
+    study_above_10_count = df_above_80[df_above_80['weekly_self_study_hours'] > 10].shape[0]
+    study_below_10_count = df_above_80[df_above_80['weekly_self_study_hours'] <= 10].shape[0]
+    
+    # Các nhóm và số lượng
+    categories = ['Học > 10 giờ', 'Học <= 10 giờ']
+    counts = [study_above_10_count, study_below_10_count]
+    
+    # Vẽ biểu đồ cột
+    plt.figure(figsize=(8, 6))
+    plt.bar(categories, counts, color=['green', 'blue'])
+    
+    # Thêm tiêu đề và nhãn cho các trục
+    plt.title('Số lượng học sinh có điểm >80 và giờ học cuối tuần trên/dưới 10h', fontsize=14)
+    plt.xlabel('Thời gian tự học', fontsize=12)
+    plt.ylabel('Số sinh viên', fontsize=12)
+    
+    # Hiển thị biểu đồ
     plt.show()
 
 # Giao diện chính
@@ -78,5 +105,6 @@ def menu_display_plot_data(data):
     tk.Button(root, text="Phân bố điểm trung bình của học sinh", command=lambda: show_chart1(data), width=34).pack(pady=10)
     tk.Button(root, text="Biểu đồ phân bố điểm dựa trên việc làm thêm", command=lambda: show_chart2(data), width=34).pack(pady=10)
     tk.Button(root, text="Phân bố số lượng học sinh có điểm trên 80", command=lambda: show_chart3(data), width=34).pack(pady=10)
+    tk.Button(root, text="Biểu đồ thể hiện sự ảnh hưởng của việc tự học", command=lambda: show_chart4(data), width=34).pack(pady=10)
     tk.Button(root, text="Thoát", command=root.destroy).pack(pady=10)
     root.mainloop()
